f1d6b581492da6108481e25c52cfcf34f13f5bcb
darkestinkgames <113227875+darkestinkgames@users.noreply.github.com>
2022-10-21 14:55:20 +0300

    upd 2

--- code/map.lua
+++ code/map.lua
@@ -5,7 +5,7 @@ math.randomseed(os.time())
 local tWidth, tHeight = 64, 48
 local width, height
 
-local tGrid
+local cGrid
 local uGrid
 
 local cSelected
@@ -14,21 +14,67 @@ local uSelected
 
 -- -- -- >>> енумерашки
 
-local color_name = {}
-
-
--- -- -- >>> нове
-
-local function newCell()end
-local function newUnit()end
-local function newPathPt()end
+local color_name = {
+  white = { 1, 1, 1 },
+}
 
 
 -- -- -- >>> функціонал
 
-local function toKey(x,y)end
-local function toGrid(sx,sy)end
-local function toScreen(gx,gy)end
+local function toKey(x,y)
+  return ("x%sy%s"):format(x,y)
+end
+local function toGrid(sx,sy)
+  return math.floor(sx / tWidth + 1), math.floor(sy / tHeight + 1)
+end
+local function toScreen(gx,gy)
+  return (gx - 1) * tWidth, (gy - 1) * tHeight
+end
+
+
+-- -- -- >>> нове
+
+local function newCell(gx,gy, name)
+  ---@class map.cell
+  local obj = {
+    key   = toKey(gx, gy),
+    tile  = name,  ---@type "plain"|"water"
+    unit  = nil,   ---@type map.unit
+  }
+  obj.gx, obj.gy = gx, gy
+  obj.sx, obj.sy = toScreen(gx, gy)
+  obj.hx, obj.hy = toScreen(gx + .5, gy + .5)
+  return obj
+end
+local function newUnit(team, mov, cell)
+  assert(team,         ("Wrong argument %s!"):format(team))
+  assert(mov,          ("Wrong argument %s!"):format(mov))
+  assert(cell,         ("Wrong argument %s!"):format(cell))
+  assert(cGrid[cell],  ("The `cell` %s is missing!"):format(cell.key))
+  ---@class map.unit
+  local obj = {
+    team   = team,  ---@type number
+    mov    = mov,   ---@type number
+    cell   = cell,  ---@type map.cell
+    pgrid  = nil,   ---@type table<string, map.pathpoint>
+    plist  = nil,   ---@type map.pathpoint[]
+  }
+  return obj
+end
+local function newPathPt(cell, val)
+  local key = cell.key
+  ---@class map.pathpoint
+  local obj = {
+    key   = key,   ---@type string
+    cell  = cell,  ---@type map.cell
+    val   = val,   ---@type number
+    from  = nil,   ---@type map.pathpoint
+  }
+  return obj
+end
+
+
+-- -- -- >>> функціонал
 
 local function setColor(name)end
 local function setPathPtVal(pp,val,from)end
