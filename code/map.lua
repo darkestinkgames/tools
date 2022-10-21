7746e2ada92cc2aab94170db145494af7768caea
darkestinkgames <113227875+darkestinkgames@users.noreply.github.com>
2022-10-21 15:58:31 +0300

    upd 3

--- code/map.lua
+++ code/map.lua
@@ -5,8 +5,8 @@ math.randomseed(os.time())
 local tWidth, tHeight = 64, 48
 local width, height
 
-local cGrid
-local uGrid
+local cGrid  ---@type table<string, map.cell>
+-- local uGrid  ---@type table<string, map.unit>
 
 local cSelected
 local uSelected
@@ -16,6 +16,9 @@ local uSelected
 
 local color_name = {
   white = { 1, 1, 1 },
+
+  plain = { .1, .25, .15 },
+  water = { .1, .15, .25 },
 }
 
 
@@ -35,22 +38,25 @@ end
 -- -- -- >>> нове
 
 local function newCell(gx,gy, name)
+  local key = toKey(gx, gy)
+  -- print(key)
   ---@class map.cell
   local obj = {
-    key   = toKey(gx, gy),
+    key   = key,
     tile  = name,  ---@type "plain"|"water"
     unit  = nil,   ---@type map.unit
   }
   obj.gx, obj.gy = gx, gy
   obj.sx, obj.sy = toScreen(gx, gy)
   obj.hx, obj.hy = toScreen(gx + .5, gy + .5)
+  obj.tile = obj.tile or "plain"
   return obj
 end
 local function newUnit(team, mov, cell)
-  assert(team,         ("Wrong argument %s!"):format(team))
-  assert(mov,          ("Wrong argument %s!"):format(mov))
-  assert(cell,         ("Wrong argument %s!"):format(cell))
-  assert(cGrid[cell],  ("The `cell` %s is missing!"):format(cell.key))
+  assert( team,      ("Wrong argument `team` (%s)!"):format(team)    )
+  assert( mov,       ("Wrong argument `mov` (%s)!"):format(mov)      )
+  assert( cell,      ("Wrong argument `cell` (%s)!"):format(cell)    )
+  assert( cell.unit, ("The `cell` %s is occupied!"):format(cell.key) )
   ---@class map.unit
   local obj = {
     team   = team,  ---@type number
@@ -62,6 +68,7 @@ local function newUnit(team, mov, cell)
   return obj
 end
 local function newPathPt(cell, val)
+  assert( cell, ("Wrong argument `cell` (%s)!"):format(cell) )
   local key = cell.key
   ---@class map.pathpoint
   local obj = {
@@ -76,18 +83,56 @@ end
 
 -- -- -- >>> функціонал
 
-local function setColor(name)end
-local function setPathPtVal(pp,val,from)end
+local function setColor(name)
+  assert( color_name[name], ("Color `%s` is missing!"):format(name) )
+  love.graphics.setColor(color_name[name])
+end
+local function setPtVal(pp,val,from) ---@param pp map.pathpoint
+  assert( pp,  ("Wrong argument `pp` (%s)!"):format(pp)   )
+  assert( val, ("Wrong argument `val` (%s)!"):format(val) )
+  if not pp.val then pp.val = val + 1 end
+  if pp.val > val then
+    pp.val = val
+    pp.from = from
+    return pp.cell
+  end
+  return nil
+end
 
-local function getRadius(val)end
-local function getCell(gx,gy)end
-local function getCost(tile)end
-local function getPt(grid,tile)end
+local function getRadius(k)
+  assert( k, ("Wrong argument `k` (%s)!"):format(k) )
+  local val = math.floor(math.min(tWidth, tHeight) * .5) * k
+  return val, val * 4
+end
+local function getCell(gx,gy)
+  return cGrid[toKey(gx,gy)]
+end
+local function getCost(cell) ---@param cell map.cell
+  assert( cell, ("Wrong argument `cell` (%s)!"):format(cell) )
+  if cell.unit then return nil end
+  if cell.tile == "water" then return 2 end
+  return 1
+end
+local function getPt(ppGrid,cell) ---@param cell map.cell
+  assert( ppGrid, ("Wrong argument `ppGrid` (%s)!"):format(ppGrid) )
+  assert( cell,   ("Wrong argument `cell` (%s)!"):format(cell)     )
+  return ppGrid[cell.key]
+end
 
 local function initPathGrid(pp,mov)end
 
-local function drawTile()end
-local function drawHover()end
+local function drawTile(cell) ---@param cell map.cell
+  assert( cell,   ("Wrong argument `cell` (%s)!"):format(cell)     )
+  setColor(cell.tile)
+  love.graphics.rectangle("fill", cell.sx,cell.sy, tWidth-1, tHeight-1)
+end
+local function drawHover()
+  local cell = getCell(toGrid(love.mouse.getPosition()))
+  if cell then
+    love.graphics.setColor(1,1,1, .5)
+    love.graphics.rectangle("fill", cell.sx,cell.sy, tWidth-1, tHeight-1)
+  end
+end
 local function drawUnit()end
 local function drawPathGrid(unit)end
 local function drawPathLine(pp)end
@@ -100,7 +145,17 @@ local function addQueue(unit,tile)end
 
 local main = {}
 
-function main.new(w,h)end
+function main.new(w,h)
+  assert( w, ("Wrong argument `w` (%s)!"):format(w) )
+  assert( h, ("Wrong argument `h` (%s)!"):format(w) )
+  cGrid = {}
+  for gy = 1, h do for gx = 1, w do
+    local cell = newCell(gx,gy)
+    cGrid[cell.key] = cell
+  end end
+  -- uGrid = {}
+end
+function main.addUnit(team, mov, x,y)end
 function main.draw()end
 function main.update(dt)end
 function main.select()end
--- main.lua
+++ main.lua
@@ -1,18 +1,28 @@
+love.graphics.setBackgroundColor( .08, .08, .08 )
 
--- 
+local map = require 'code/map'
 
 -- -- -- >>>
 
 function love.load(...)
+  map.new(10,8)
+  map.addUnit(1, 4)
+  map.addUnit(1, 5)
+  map.addUnit(2, 4)
+  map.addUnit(2, 5)
 end
 function love.draw()
+  map.draw()
 end
 function love.update(dt)
+  map.update(dt)
 end
 function love.keypressed(key)
   if key == 'escape' then love.event.push('quit') end
 end
 function love.mousepressed(x, y, button, istouch, presses)
+  if button == 1 then map.select() end
+  if button == 2 then map.deselect() end
 end
 function love.wheelmoved( x , y )
 end
