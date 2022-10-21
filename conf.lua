b4a91323dc6c28cce6b91344ac14605359abf320
darkestinkgames <113227875+darkestinkgames@users.noreply.github.com>
2022-10-21 13:09:15 +0300

    upd 0

---
+++ conf.lua
@@ -0,0 +1,9 @@
+function love.conf(t)
+  -- t.version = 11.4
+  t.window.resizable = true
+  t.window.title = ('NoName // LÖVE 2D ver. %s'):format(t.version)
+  -- t.window.width = nil
+  -- t.window.height = nil
+  t.window.minwidth = 400
+  t.window.minheight = 200
+end
---
+++ main.lua
@@ -0,0 +1,25 @@
+
+-- 
+
+-- -- -- >>>
+
+function love.load(...)
+end
+function love.draw()
+end
+function love.update(dt)
+end
+function love.keypressed(key)
+  if key == 'escape' then love.event.push('quit') end
+end
+function love.mousepressed(x, y, button, istouch, presses)
+end
+function love.wheelmoved( x , y )
+end
+function love.resize(w, h)
+end
+
+
+-- -- -- >>>
+
+-- 
