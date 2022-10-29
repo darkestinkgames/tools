7eae33576f73608fb7ca7c82e554ba97ddcb4a8e
darkestinkgames <113227875+darkestinkgames@users.noreply.github.com>
2022-10-29 22:06:47 +0300

    upd 28
    трохи перетасував те, що мав
    всеодно неподобається

--- main.lua
+++ main.lua
@@ -1,16 +1,16 @@
 love.graphics.setBackgroundColor( .08, .08, .08 )
 
-local map = require 'code/map'
+local map = require 'code/map2'
 
 -- -- -- >>>
 
 function love.load(...)
-  map.tileRate(4, 1)
+  -- map.tileRate(4, 1)
   map.newGridRandom(12,12)
-  map.addUnit(1, 4)
-  map.addUnit(1, 5)
-  map.addUnit(2, 4)
-  map.addUnit(2, 5)
+  -- map.addUnit(1, 4)
+  -- map.addUnit(1, 5)
+  -- map.addUnit(2, 4)
+  -- map.addUnit(2, 5)
 end
 function love.draw()
   map.draw()
