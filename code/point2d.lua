2a93aa8f1e1caabf814c2aaa5bb7df19f485c690
darkestinkgames <113227875+darkestinkgames@users.noreply.github.com>
2022-11-03 03:16:09 +0200

    upd 37

--- code/point2d.lua
+++ code/point2d.lua
@@ -1,26 +1,67 @@
 
 local point2d = {}
 
----@class mod.point
-local Point = {
+local Point = { ---@class mod.Point
   x = 0,
   y = 0,
 }
 
 local mtPoint = {}
 
--- 
-
-local function fn()end
 
 -- 
 
-function Point:fn() end
+local function check(a, b)end
+
+local function add(a, b)end
+local function sub(a, b)end
+local function mul(a, b)end
+local function div(a, b)end
+local function mod(a, b)end
+local function pow(a, b)end
 
 -- 
 
-function point2d:fn() end
+function Point:get() ---@return number, number
+  return self.x, self.y
+end
+
+function Point:getF() ---@return number, number
+  return math.floor(self.x), math.floor(self.y)
+end
+
+function Point:set(x,y)
+  self.x,self.y = x,y
+end
+
+
+-- 
+
+function point2d.new(x,y) ---@return mod.Point
+  return setmetatable({
+    x = x,
+    y = y,
+  }, mtPoint)
+end
+
+function point2d.length(a,b) ---@return number
+end
+
+function point2d.radian(a,b) ---@return number
+end
+
+function point2d.degree(a,b) ---@return number
+end
+
 
 -- 
 
 return point2d
+
+---@class mod.Point
+---@operator add:mod.Point
+---@operator sub:mod.Point
+---@operator mul:mod.Point
+---@operator div:mod.Point
+---@operator mod:mod.Point
+---@operator pow:mod.Point
--- main.lua
+++ main.lua
@@ -1,24 +1,24 @@
 love.graphics.setBackgroundColor( .08, .08, .08 )
 
-local map = require 'code/map3'
+local point2d = require 'code/point2d'
+
+local a = point2d.new(5,5)
+local b = point2d.new(7,7)
+local c = a + b - 2 * 2 / 2 ^ 2
+
 
 -- -- -- >>>
 
 function love.load(...)
-  map.random(12,12)
 end
 function love.draw()
-  map.draw()
 end
 function love.update(dt)
-  map.update(dt)
 end
 function love.keypressed(key)
   if key == 'escape' then love.event.push('quit') end
 end
 function love.mousepressed(x, y, button, istouch, presses)
-  if button == 1 then map.select() end
-  if button == 2 then map.deselect() end
 end
 function love.wheelmoved( x , y )
 end
