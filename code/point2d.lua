b817d6ef185e2606b864c5ca30efeafe73651473
darkestinkgames <113227875+darkestinkgames@users.noreply.github.com>
2022-11-03 02:45:35 +0200

    upd 36
    трохи відволікусь від основної мети

---
+++ code/point2d.lua
@@ -0,0 +1,26 @@
+
+local point2d = {}
+
+---@class mod.point
+local Point = {
+  x = 0,
+  y = 0,
+}
+
+local mtPoint = {}
+
+-- 
+
+local function fn()end
+
+-- 
+
+function Point:fn() end
+
+-- 
+
+function point2d:fn() end
+
+-- 
+
+return point2d
