e89eb070a7f149b70801ba7ed18b917903e56879
darkestinkgames <113227875+darkestinkgames@users.noreply.github.com>
2023-02-13 11:53:43 +0200

    u60
    1) сорян, я замахався занотовувати прогрес (та й, схоже, нікому воно не цікаве)
    
    2) тут буду складати корисні (на мою думку) штуки, усілякий побічний продукт

--- conf.lua
+++
@@ -1,9 +0,0 @@
-function love.conf(t)
-  -- t.version = 11.4
-  t.window.resizable = true
-  t.window.title = ('NoName // %s / LÖVE %s'):format(_VERSION, t.version)
-  -- t.window.width = nil
-  -- t.window.height = nil
-  t.window.minwidth = 400
-  t.window.minheight = 200
-end
